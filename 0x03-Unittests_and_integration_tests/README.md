2aaf0d7d256ce911d31fe251e285cfdac9dad472
Amanuel2000 <amanuelf44@gmail.com>
2023-05-21 11:22:06 +0300

    Unittests and integrtation test: test utils

---
+++ 0x03-Unittests_and_integration_tests/README.md
@@ -0,0 +1,1 @@
+## Ubnittests and Intergration tests
---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+
+"""
+Parameterize a unit test 
+wirting  a unit test for utils.access_nested_map
+
+
+"""
+from parameterized import parameterized
+from typing import (
+    Mapping,
+    Sequence,
+    Dict,
+    Any
+)
+import unittest
+from unittest.mock import patch
+
+import utils
+from utils import access_nested_map, get_json
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """
+    Class for testing access_nested_map function
+    """
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a"), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(
+                               self,
+                               nested_map: Mapping,
+                               path: Sequence,
+                               expected_output: Any
+                               ) -> None:
+        """
+        Test access_nested_map function
+        """
+        self.assertEqual(access_nested_map(nested_map, path), expected_output)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(
+                               self,
+                               nested_map: Mapping,
+                               path: Sequence,
+                               ) -> None:
+        """
+        Test access_nested_map function with exception
+        """
+        with self.assertRaises(KeyError) as e:
+            access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """
+    Class for testing get_json function
+    """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(
+                      self,
+                      test_url: str,
+                      test_payload: Dict[str, bool]
+                      ) -> None:
+        """
+        Test get_json function
+        """
+        with patch("utils.requests.get") as mocked_get:
+            mocked_get.return_value.json.return_value = test_payload
+            self.assertEqual(get_json(test_url), test_payload)
+            mocked_get.assert_called_once_with(test_url)
+
+
+if __name__ == '__main__':
+    unittest.main()
